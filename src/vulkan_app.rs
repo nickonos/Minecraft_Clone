70c1795f5ecf3f0c018d605b6b4b782a55511a17
nick van Oijen <I468166@fhict.nl>
2022-01-21 16:18:19 +0100

    app creation

---
+++ src/vulkan_app.rs
@@ -0,0 +1,132 @@
+use winit::event::{Event, ElementState, KeyboardInput, WindowEvent, VirtualKeyCode};
+use winit::event_loop::{EventLoop, ControlFlow};
+use ash::version::{InstanceV1_0, EntryV1_0};
+use std::ffi::CString;
+use ash::vk;
+use std::ptr;
+use winit::window::Window;
+
+use crate::utilities;
+
+const WINDOW_TITLE: &'static str = "Minecraft";
+const WINDOW_HEIGHT: u32 = 600;
+const WINDOW_WIDTH: u32 = 800;
+
+pub struct VulkanApp{
+    _entry: ash::Entry,
+    instance: ash::Instance,
+}
+
+impl VulkanApp{
+
+    pub fn new() -> VulkanApp{
+        let entry = ash::Entry::new().unwrap();
+        let instance = VulkanApp::create_instance(&entry);
+
+        VulkanApp{
+            _entry: entry,
+            instance,
+        }
+    }
+
+    pub fn run(){
+        let event_loop = EventLoop::new();
+        let window = VulkanApp::init_window(&event_loop);
+
+        let vulkan_app = VulkanApp::new();
+        vulkan_app.main_loop(event_loop, window);
+    }
+
+
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window{
+        winit::window::WindowBuilder::new()
+            .with_title(WINDOW_TITLE)
+            .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
+            .build(event_loop)
+            .expect("Failed to create window")
+    }
+
+    fn create_instance(entry: &ash::Entry) -> ash::Instance{
+        let app_name = CString::new(WINDOW_TITLE).unwrap();
+        let engine_name = CString::new("Vulkan Engine").unwrap();
+        let app_info = vk::ApplicationInfo {
+            s_type: vk::StructureType::APPLICATION_INFO,
+            p_next: ptr::null(),
+            p_application_name: app_name.as_ptr(),
+            application_version: APPLICATION_VERSION,
+            p_engine_name: engine_name.as_ptr(),
+            engine_version: ENGINE_VERSION,
+            api_version: API_VERSION,
+        };
+
+        let extension_names = utilities::platform_utilities::required_extension_names();
+
+        let create_info = vk::InstanceCreateInfo {
+            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::InstanceCreateFlags::empty(),
+            p_application_info: &app_info,
+            pp_enabled_layer_names: ptr::null(),
+            enabled_layer_count: 0,
+            pp_enabled_extension_names: extension_names.as_ptr(),
+            enabled_extension_count: extension_names.len() as u32,
+        };
+
+        let instance: ash::Instance = unsafe {
+            entry
+                .create_instance(&create_info, None)
+                .expect("Failed to create instance!")
+        };
+
+        instance
+    }
+
+    fn draw_frame(&mut self) {
+        // Drawing will be here
+    }
+
+
+    fn main_loop(mut self, event_loop: EventLoop<()>, window: Window){
+        event_loop.run(move |event, _, control_flow| {
+
+            match event {
+                | Event::WindowEvent { event, .. } => {
+                    match event {
+                        | WindowEvent::CloseRequested => {
+                            *control_flow = ControlFlow::Exit
+                        },
+                        | WindowEvent::KeyboardInput { input, .. } => {
+                            match input {
+                                | KeyboardInput { virtual_keycode, state, .. } => {
+                                    match (virtual_keycode, state) {
+                                        | (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                                            *control_flow = ControlFlow::Exit
+                                        },
+                                        | _ => {},
+                                    }
+                                },
+                            }
+                        },
+                        | _ => {},
+                    }
+                },
+                | Event::MainEventsCleared => {
+                    window.request_redraw();
+                },
+                | Event::RedrawRequested(_window_id) => {
+                    self.draw_frame();
+                },
+                _ => (),
+            }
+        })
+    }
+}
+
+impl Drop for VulkanApp{
+    fn drop(&mut self){
+        unsafe{
+            self.instance.destroy_instance(None)
+        }
+    }
+}
+
